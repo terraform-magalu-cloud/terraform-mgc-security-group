c695e1d160a230f23c30c073367a95cef592a61e
Antonio Novaes <antonionovaesjr@gmail.com>
2024-05-25 23:39:50 -0300

    feat: some outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
-output "name" {
-  value = var.name
+output "sg_name" {
+  value = mgc_network_security_groups.this[0].name
+}
+
+output "sg_id" {
+  value = mgc_network_security_groups.this[0].id
+}
+
+output "sg_rules" {
+  value = mgc_network_security_groups.this[0].rules
+}
+
+output "create_at" {
+  value = mgc_network_security_groups.this[0].create_at
+  
 }
