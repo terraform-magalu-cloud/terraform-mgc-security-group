c33cc5b5f52ee7c9f6bb078dd9e022b7a2c8bfc6
Antonio Novaes <antonionovaesjr@gmail.com>
2024-05-26 17:19:02 -0300

    feat: add description example

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,13 +1,16 @@
 output "name" {
+  description = "name of sg"
   value = module.example.name
 }
 
 output "id" {
+  description = "id of sg"
   value = module.example.id
 
 }
 
 output "rules" {
+  description = "list rules sg"
   value = module.example.rules
 
 }
