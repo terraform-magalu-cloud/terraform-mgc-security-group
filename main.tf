a0bc2e5a40c82b0e612032b47d3b51eee1a951ca
Antonio Novaes <antonionovaesjr@gmail.com>
2025-04-21 20:57:19 -0300

    chore: remove paramenter not used in new versio

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
 resource "mgc_network_security_groups" "this" {
-  count          = var.create ? 1 : 0
-  name           = var.name
-  description    = var.description
-  validate_quota = true
+  count       = var.create ? 1 : 0
+  name        = var.name
+  description = var.description
 }
 
-resource "mgc_network_rules" "this" {
+resource "mgc_network_security_groups_rules" "this" {
+  count             = var.create ? length(var.rules) : 0
   depends_on        = [mgc_network_security_groups.this]
-  for_each          = var.create ? var.rules : {}
+  description       = var.rules[count.index].description
   security_group_id = mgc_network_security_groups.this[0].id
-  direction         = each.value.is_egress == false ? "ingress" : "egress"
-  ethertype         = tonumber(each.value.ipv) == 4 || each.value.ipv == 6 ? "IPv${each.value.ipv}" : "ERRO: value not support, use only 4 or 6"
-  remote_ip_prefix  = each.value.source_cidr
-  protocol          = each.value.protocol == "tcp" || each.value.protocol == "udp" || each.value.protocol == "icmp" ? each.value.protocol : "ERRO: value not support, use only tcp, udp or icmp"
-  port_range_min    = tonumber(each.value.port_min)
-  port_range_max    = tonumber(each.value.port_max)
-  validate_quota    = var.validate_quota
-  wait              = var.wait
+  direction         = var.rules[count.index].egress == false ? "ingress" : "egress"
+  ethertype         = tonumber(var.rules[count.index].ipv) == 4 || var.rules[count.index].ipv == 6 ? "IPv${var.rules[count.index].ipv}" : "ERRO: value not support, use only 4 or 6"
+  remote_ip_prefix  = var.rules[count.index].cidr
+  protocol          = var.rules[count.index].protocol == "tcp" || var.rules[count.index].protocol == "udp" || var.rules[count.index].protocol == "icmp" ? var.rules[count.index].protocol : "ERRO: value not support, use only tcp, udp or icmp"
+  port_range_min    = tonumber(var.rules[count.index].port_min)
+  port_range_max    = tonumber(var.rules[count.index].port_max)
 }
